56211f91c29f576eec1a2e74e1f4af175f8a606e
Bernard Olivier <bernardolivierbpo@gmail.com>
2023-03-24 23:01:02 +0200

    Creating login screen

--- index.php
+++ index.php
@@ -1,22 +1,9 @@
-<?php
+<!DOCTYPE html>
+<html land="en">
 
-$connect = mysql_connect(
-    'db',
-    'user',
-    'password',
-    'my_movie_list'
-);
-
-$query = 'SELECT * FROM a_table';
-$result = mysql_query($connect, $query);
-
-echo '<h1>MySQL Content:</h1>';
-
-while($record = mysql_fetch_assoc($result)) {
-    echo '<h2>'.$record['title'].'</h2>';
-    echo '<p>'.$record['title'].'</p>';
-    echo 'List: ' .$record['title'];
-    echo '<hr>';
-}
-
-//Database table still needs to be created
+<head>
+    <title>My phpserver.browser List</title>
+    <meta charset="UTF-8">
+</head>
+    <h1> Hello World </h1>
+</html>
