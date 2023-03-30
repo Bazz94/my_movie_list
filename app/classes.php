3b41efacebac6b05875f165e9b597775215f2b34
Bernard Olivier <88403974+Bazz94@users.noreply.github.com>
2023-03-30 23:31:35 +0200

    Implemented getting posters from db

--- app/classes.php
+++ app/classes.php
@@ -1,17 +1,23 @@
 <?php 
 class Movie {
-  public $id;
-  public $title;
-  public $date;
+  private $id;
+  private $title;
+  private $date;
+  private $poster;
 
-  function __construct($id, $title, $date) {
+  function __construct($id, $title, $date, $poster) {
     $this->id = $id;
     $this->title = $title;
     $this->date = $date;
+    $this->poster = $poster;
   }
   
   function to_string() {
     return $this->title . ' (' . $this->date . ')';
+  }
+
+  function getPoster() {
+    return '../movie_posters/' . $this->poster;
   }
 }
 ?>
