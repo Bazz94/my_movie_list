cbeccba79834067832ae2ff9bf30603986c15135
Bernard Olivier <88403974+Bazz94@users.noreply.github.com>
2023-04-15 21:34:54 +0200

    Minor fixes

--- app/create_user.php
+++ app/create_user.php
@@ -1,53 +1,59 @@
 <?php
-// Get constants
 require('../app/constants.php');
-// Try and connect using the info above.
-$connection = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-if ($connection->connect_error) {
-  // If there is an error with the connection, stop the script and display the error.
-  $_SESSION['error'] = 'Failed to connect to MySQL: ' . $connection->connect_error;
-  exit();
-}
 
-// Now we check if the data from the login form was submitted, isset() will check if the data exists.
+// Check to see if the required data was submitted
 if (!isset($_POST['email'], $_POST['username'], $_POST['password'], $_POST['passwordCheck'])) {
   // Could not get the data that should have been sent.
-  $_SESSION['error'] = 'Failed to retrieve form data: ';
-  exit();
+  $_SESSION['error'] = 'Failed to retrieve form data';
+  header('Location: signup.php');
 }
 
-$password = crypt($_POST['password'],'2394a9661a9089208c1c9c65ccac85a91da6a859');
+// Connect to database
+try {
+  $connection = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+} catch (mysqli_sql_exception $e) {
+  // If there is an error with the connection, stop the script and display the error.
+  $_SESSION['error'] = 'Failed to connect to User Database';
+  // $e->getMessage();
+  header('Location: error.php');
+}
+
+//Hash password
+$password = password_hash($_POST['password'], PASSWORD_BCRYPT);
 
 // Prepare our SQL, preparing the SQL statement will prevent SQL injection.
-$stmt = $connection->prepare("INSERT INTO `users` (`userid`, `username`, `_password`, `email`) 
+$stmt = $connection->prepare("INSERT INTO users (`user_id`, `username`, `password`, `email`) 
   VALUES (UUID_SHORT(), ?, ?, ?) ");
 
-if (!$stmt) {
-    $_SESSION['error'] = "Error: " . mysqli_error($connection);
-    exit();
-}
-
-// Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
+// Bind parameters (s for string)
 $stmt->bind_param('sss', $_POST['username'], $password, $_POST['email']);
+
+//check for prepare statement errors
+if (!$stmt) {
+    $_SESSION['error'] = "Error preparing sql statement";
+    // mysqli_error($connection)
+    header('Location: error.php');
+}
 
 // Execute statement
 $stmt->execute();
 
+//check for execution errors
 if ($stmt->errno) {
-    $_SESSION['error'] = "Error: " . $stmt->error;
-    exit();
+  $_SESSION['error'] = "SQL Execution Error";
+  // $stmt->error
+  header('Location: error.php');
 }
 
-// Get number of affected rows
-$num_rows = $stmt->affected_rows;
-if ($num_rows != 1){
-  $_SESSION['error'] = "Error: affected rows incorrect " . $num_rows;
-  exit();
+// Check to see if records were added
+if ($stmt->affected_rows < 1){
+  $_SESSION['error'] = "No rows were added";
+  header('Location: error.php');
 } 
 
 $stmt->close();
 $connection->close();
 
-// Login
+// Login the created user
 include('../app/authenticate.php');
 ?>
--- app/get_movies.php
+++ app/get_movies.php
@@ -1,35 +1,55 @@
 <?php 
 require_once('../app/classes.php');
 require_once('../app/constants.php');
-
-$con = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-if (mysqli_connect_errno()) {
-  // If there is an error with the connection, stop the script and display the error.
-  $_SESSION['error'] = 'Failed to connect to MySQL: ' . mysqli_connect_error();
-  exit();
-}
-
 $community_Ranking = [];
 
-// Prepare our SQL, preparing the SQL statement will prevent SQL injection.
-if ($stmt = $con->prepare('SELECT movieid, title, _date, poster FROM movies ORDER BY _weight DESC, title ASC LIMIT 100')) {
-  $stmt->execute();
-  // Store the result so we can check if the account exists in the database.
-  $stmt->store_result();
-
-  if ($stmt->num_rows > 0) {
-    //records do exist
-    $stmt->bind_result($movieid, $title, $date, $poster);
-    while ($stmt->fetch()) {
-      $community_Ranking[] = new Movie($movieid, $title, $date, $poster);
-    }
-  } else {
-    $_SESSION['error'] = 'There are no movies available';
-  }
-  $stmt->close();
-} else {
-  $_SESSION['error'] = 'Error preparing SQL statement: ' . mysqli_error($con);
+//connect to database
+try {
+  $connection = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+} catch (mysqli_sql_exception $e) {
+  // If there is an error with the connection, stop the script and display the error.
+  $_SESSION['error'] = 'Failed to connect to Movies Database';
+  // $e->getMessage();
+  header('Location: error.php');
 }
 
+// Prepare our SQL, preparing the SQL statement will prevent SQL injection.
+$stmt = $connection->prepare('SELECT `movie_id`, `title`, `date`, `poster` FROM movies ORDER BY `weight` DESC, `title` ASC LIMIT 100');
+
+//check for errors
+if (!$stmt) {
+    $_SESSION['error'] = "Error preparing sql statement: " . mysqli_error($connection);
+    header('Location: error.php');
+}
+
+// Execute statement
+$stmt->execute();
+
+//check for execution errors
+if ($stmt->errno) {
+  $_SESSION['error'] = "SQL Execution Error: " . $stmt->error;
+  header('Location: error.php');
+}
+
+//store to use data
+$stmt->store_result();
+
+//Check to see if any rows were affected
+if ($stmt->num_rows < 1) {
+  $_SESSION['error'] = 'There are no movies available';
+  header('Location: error.php');
+}
+
+// Set results to an array
+$stmt->bind_result($movieid, $title, $date, $poster);
+while ($stmt->fetch()) {
+  $community_Ranking[] = new Movie($movieid, $title, $date, $poster);
+}
+
+//Add to session data
 $_SESSION['community-movies'] = $community_Ranking;
+
+//close connections 
+$stmt->close();
+$connection->close();
 ?>
